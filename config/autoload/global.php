9f0acb239e9df467b028800518bfd80a790cb804
lukasz <lukasz.rozanski@gmail.com>
2016-02-07 21:46:20 +0100

    initial commit

---
+++ config/autoload/global.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Global Configuration Override
+ *
+ * You can use this file for overriding configuration values from modules, etc.
+ * You would place values in here that are agnostic to the environment and not
+ * sensitive to security.
+ *
+ * @NOTE: In practice, this file will typically be INCLUDED in your source
+ * control, so do not include passwords or other sensitive information in this
+ * file.
+ */
+
+ return array(
+       'navigation' => array(
+           'default' => array(
+               array(
+                   'label' => 'Home',
+                   'route' => 'home',
+               ),
+               array(
+                   'label' => 'Projekty',
+                   'route' => 'project',
+               ),
+               array(
+                   'label' => 'Artefakty',
+                   'route' => 'artifact',
+
+               ),
+              //  array(
+              //      'label' => 'Zgłaszanie problemów',
+              //      'route' => 'issue'
+              //  ),
+               array(
+                   'label' => 'Użytkownicy',
+                   'route' => 'user'
+               ),
+               array(
+                   'label' => 'Ustawienia',
+                   'route' => 'setting'
+               ),
+               array(
+                   'label' => 'O serwisie',
+                   'route' => 'serwis'
+               )
+           ),
+       ),
+       'service_manager' => array(
+           'factories' => array(
+               'navigation' => 'Zend\Navigation\Service\DefaultNavigationFactory',
+           ),
+       ),
+   );
